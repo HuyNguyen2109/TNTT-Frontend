4f57bfbd3861070a0089926b7eaf6e14006c5d97
Nguyen Nhut Huy <johnasHuy21091996@gmail.com>
2019-10-03 16:49:03 +0700

    Implement Forgot Account Dialog

--- tntt/src/App.js
+++ tntt/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignIn from './Login';
 import SignUp from './Signup';
+import Temp from './tempComponent';
 
 function App() {
   return (
     <Router>
       <Route path="/" exact component={SignIn} />
       <Route path="/dang-ki" component={SignUp} />
+      <Route path="/temp" component={Temp}/>
     </Router>
   );
 }

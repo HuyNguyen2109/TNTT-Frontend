882ad2927a03d557ff5dec0e05401a6934cbc189
Nguyen Nhut Huy <johnasHuy21091996@gmail.com>
2019-10-08 16:45:36 +0700

    Finish "Remember Me" and re-structure project

--- tntt/src/App.js
+++ tntt/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import SignIn from './Login';
-import SignUp from './Signup';
-import Temp from './tempComponent';
+import SignIn from './components/Login';
+import SignUp from './components/Signup';
+import Temp from './components/tempComponent';
 
 function App() {
   return (

eebc1cde07608174588e29570045d87a6f4b2142
Nguyen Nhut Huy <johnasHuy21091996@gmail.com>
2019-11-14 15:52:21 +0700

    Implement Add new Button

--- tntt/src/components/Dashboard/views/Dashboard/Dashboard.js
+++ tntt/src/components/Dashboard/views/Dashboard/Dashboard.js
@@ -3,6 +3,9 @@ import { withStyles } from '@material-ui/styles';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import axios from 'axios';
 import {
+  AddCircle
+} from '@material-ui/icons/';
+import {
   Card,
   CardContent,
   Table,
@@ -13,7 +16,9 @@ import {
   TablePagination,
   CardActions,
   Grid,
-  Button
+  Button,
+  TextField,
+  Collapse
 } from '@material-ui/core';
 import clsx from 'clsx';
 
@@ -60,12 +65,29 @@ class Dashboard extends React.Component {
       selectedRecord: [],
       numberOfRecord: 0,
       currentClass: '',
+      isExpansionButton: false,
+
+      newFirstname: '',
+      newLastName: '',
+      newFatherName: '',
+      newMotherName: '',
+      newDiocese: '',
+      newgender: '',
+      newClass: '',
+      newBirthday: '',
+      newDayOfBaptism: '',
+      newDayofEucharist: '',
+      newDayofConfirmation: '',
     }
   }
 
   componentDidUpdate = (prevProps) => {
-    if(this.props.location.pathname.split("/")[2] !== prevProps.location.pathname.split("/")[2]) {
-      return this.getData(null, this.state.tablePage, this.state.itemPerPage) && this.getNumberOfRecord();
+    if (this.props.location.pathname.split("/")[2] !== prevProps.location.pathname.split("/")[2]) {
+      this.setState({
+        tablePage: 0
+      });
+
+      return this.getData(null, 0, this.state.itemPerPage) && this.getNumberOfRecord();
     }
   }
 
@@ -74,51 +96,50 @@ class Dashboard extends React.Component {
   }
 
   getNumberOfRecord = () => {
-    if(this.props.location.pathname !== '/dashboard/all') 
-    {
+    if (this.props.location.pathname !== '/dashboard/all') {
       return axios
-      .get('/backend/class/by-path', {
-        params: {
-          path: this.props.location.pathname
-        }
-      })
-      .then(result => {
-        const classID = result.data.data[0].ID;
-        this.setState({
-          currentClass: result.data.data[0].Value
-        })
-        return axios
-        .get('/backend/children/count', {
+        .get('/backend/class/by-path', {
           params: {
-            condition: classID
+            path: this.props.location.pathname
           }
         })
-      })
-      .then(result => {
-        this.setState({
-          numberOfRecord: result.data.data
+        .then(result => {
+          const classID = result.data.data[0].ID;
+          this.setState({
+            currentClass: result.data.data[0].Value
+          })
+          return axios
+            .get('/backend/children/count', {
+              params: {
+                condition: classID
+              }
+            })
         })
-      })
-      .catch(err => {
-        console.log(err);
-      })
+        .then(result => {
+          this.setState({
+            numberOfRecord: result.data.data
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        })
     }
     else {
       return axios
-      .get('/backend/children/count',{
-        params: {
-          condition: 'all'
-        }
-      })
-      .then(result => {
-        this.setState({
-          numberOfRecord: result.data.data
+        .get('/backend/children/count', {
+          params: {
+            condition: 'all'
+          }
         })
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }  
+        .then(result => {
+          this.setState({
+            numberOfRecord: result.data.data
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
   }
 
   getData = (pathname, page, itemPerPage) => {
@@ -162,86 +183,138 @@ class Dashboard extends React.Component {
     return this.getData(null, page, this.state.itemPerPage);
   }
 
+  handleFormChange = (e, type) => {
+    let data = e.target.value;
+    const result = {};
+    result[type] = data;
+    this.setState(result);
+  }
+
+  toggleExpansionForm = (e) => {
+    this.setState({
+      isExpansionButton: !this.state.isExpansionButton
+    })
+  }
+
   render = () => {
     const { classes, className } = this.props;
 
     return (
       <Grid
-          container
-          className={classes.root}
-        >
-          <Grid item xs={12} style={{marginBottom: '2em'}}>
-            <div>
-              <h1>{(this.props.location.pathname.split("/")[2] === 'all')? "Danh sách chung" : `Danh sách lớp ${this.state.currentClass}`}</h1>
-            </div>
-          </Grid>
-          <Grid item xs={12}>
-            <Card
-              className={clsx(classes.root, className)}
-            >
-              <CardContent className={classes.content}>
-                <PerfectScrollbar>
-                  <div className={classes.inner}>
-                    <Table stickyHeader aria-label="sticky table">
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>Tên Thánh và Họ</TableCell>
-                          <TableCell>Tên</TableCell>
-                          <TableCell>Họ tên Cha</TableCell>
-                          <TableCell>Họ tên Mẹ</TableCell>
-                          <TableCell>Giáo khu</TableCell>
-                          <TableCell>Giới tính</TableCell>
-                          <TableCell>Sinh nhật</TableCell>
-                          <TableCell>Ngày Rửa Tội</TableCell>
-                          <TableCell>Ngày Rước Lễ</TableCell>
-                          <TableCell>Ngày Thêm Sức</TableCell>
-                          <TableCell>Địa chỉ</TableCell>
-                          <TableCell>Liên lạc</TableCell>
-                          <TableCell>Lớp</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {this.state.records.map(record => (
-                          <TableRow
-                            className={classes.tableRow}
-                            hover
-                            key={record.ID}
-                            selected={this.state.selectedRecord.indexOf(record.ID) !== -1}
-                          >
-                            <TableCell className={classes.tableCell}>{record.firstname}</TableCell>
-                            <TableCell className={classes.tableCell}>{record.lastname}</TableCell>
-                            <TableCell>{record.father_name}</TableCell>
-                            <TableCell>{record.mother_name}</TableCell>
-                            <TableCell>{record.diocese}</TableCell>
-                            <TableCell>{(record.male === "x") ? "Nam" : "Nữ"}</TableCell>
-                            <TableCell>{record.birthday}</TableCell>
-                            <TableCell>{record.day_of_baptism}</TableCell>
-                            <TableCell>{record.day_of_eucharist}</TableCell>
-                            <TableCell>{record.day_of_confirmation}</TableCell>
-                            <TableCell>{record.address}</TableCell>
-                            <TableCell>{record.contact}</TableCell>
-                            <TableCell>{record.class}</TableCell>
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </div>
-                </PerfectScrollbar>
-              </CardContent>
-              <CardActions className={classes.actions}>
-                <TablePagination
-                  component="div"
-                  count={this.state.numberOfRecord}
-                  onChangePage={(e, page) => this.handleChangePage(e, page)}
-                  onChangeRowsPerPage={e => this.handleChangeRowsPerPage(e)}
-                  page={this.state.tablePage}
-                  rowsPerPage={this.state.itemPerPage}
-                  rowsPerPageOptions={[10, 25, 50]} />
-              </CardActions>
-              <Button variant="contained" color="primary">Thêm mới</Button>
-            </Card>
-          </Grid>
+        container
+        className={classes.root}
+      >
+        <Grid item xs={12} style={{ marginBottom: '2em' }}>
+          <div>
+            <h1>{(this.props.location.pathname.split("/")[2] === 'all') ? "Danh sách chung" : `Danh sách lớp ${this.state.currentClass}`}</h1>
+          </div>
         </Grid>
+        <Grid item xs={12}>
+          <Card
+            className={clsx(classes.root, className)}
+          >
+            <CardContent className={classes.content}>
+              <PerfectScrollbar>
+                <div className={classes.inner}>
+                  <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Tên Thánh và Họ</TableCell>
+                        <TableCell>Tên</TableCell>
+                        <TableCell>Họ tên Cha</TableCell>
+                        <TableCell>Họ tên Mẹ</TableCell>
+                        <TableCell>Giáo khu</TableCell>
+                        <TableCell>Giới tính</TableCell>
+                        <TableCell>Sinh nhật</TableCell>
+                        <TableCell>Ngày Rửa Tội</TableCell>
+                        <TableCell>Ngày Rước Lễ</TableCell>
+                        <TableCell>Ngày Thêm Sức</TableCell>
+                        <TableCell>Địa chỉ</TableCell>
+                        <TableCell>Liên lạc</TableCell>
+                        <TableCell>Lớp</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {this.state.records.map(record => (
+                        <TableRow
+                          className={classes.tableRow}
+                          hover
+                          key={record.ID}
+                          selected={this.state.selectedRecord.indexOf(record.ID) !== -1}
+                        >
+                          <TableCell>{record.firstname}</TableCell>
+                          <TableCell>{record.lastname}</TableCell>
+                          <TableCell>{record.father_name}</TableCell>
+                          <TableCell>{record.mother_name}</TableCell>
+                          <TableCell>{record.diocese}</TableCell>
+                          <TableCell>{(record.male === "x") ? "Nam" : "Nữ"}</TableCell>
+                          <TableCell>{record.birthday}</TableCell>
+                          <TableCell>{record.day_of_baptism}</TableCell>
+                          <TableCell>{record.day_of_eucharist}</TableCell>
+                          <TableCell>{record.day_of_confirmation}</TableCell>
+                          <TableCell>{record.address}</TableCell>
+                          <TableCell>{record.contact}</TableCell>
+                          <TableCell>{record.class}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              </PerfectScrollbar>
+            </CardContent>
+            <CardActions className={classes.actions}>
+              <TablePagination
+                component="div"
+                count={this.state.numberOfRecord}
+                onChangePage={(e, page) => this.handleChangePage(e, page)}
+                onChangeRowsPerPage={e => this.handleChangeRowsPerPage(e)}
+                page={this.state.tablePage}
+                rowsPerPage={this.state.itemPerPage}
+                rowsPerPageOptions={[10, 25, 50]}
+                labelRowsPerPage="Số lượng dòng trên trang" />
+              <Button variant="contained" color="primary" onClick={e => this.toggleExpansionForm(e)}><AddCircle fontSize="small" /> Thêm mới</Button>
+            </CardActions>
+
+            <Collapse in={this.state.isExpansionButton}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    label="Tên thánh và Họ"
+                    name="firstName"
+                    id="firstName"
+                    value={this.state.newFirstname}
+                    onChange={e => this.handleFormChange(e, "newFirstname")}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    label="Tên"
+                    name="lastName"
+                    id="lastName"
+                    value={this.state.newLastName}
+                    onChange={e => this.handleFormChange(e, "newLastName")}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                  <TextField
+                    required
+                    label="Họ tên Cha"
+                    name="fatherName"
+                    id="fatherName"
+                    value={this.state.newFatherName}
+                    onChange={e => this.handleFormChange(e, "newFatherName")}
+                    fullWidth
+                  />
+                </Grid>
+              </Grid>
+            </Collapse>
+          </Card>
+        </Grid>
+      </Grid>
     );
   }
 }
